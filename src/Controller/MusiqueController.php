6c36eec8fab21741e6a45efdb5aac8031778a0f5
jahleel <vabyz971@gmail.com>
2019-09-07 10:22:41 -0400

    Insertion des donnée
    Ajout des informations pour la page musique

--- src/Controller/MusiqueController.php
+++ src/Controller/MusiqueController.php
@@ -5,7 +5,12 @@ namespace App\Controller;
 use App\Entity\Musique;
 use App\Repository\MusiqueRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
 
 
 class MusiqueController extends AbstractController
@@ -25,13 +30,44 @@ class MusiqueController extends AbstractController
     }
 
 
+     /**
+     * @Route("/musique/new", name="musique_created")
+     */
+    public function create(Request $request, ObjectManager $manager){
+
+        $musique = new Musique();
+
+        //Creation d'un formulaire
+        $form = $this->createFormBuilder($musique)
+            ->add('title')
+            ->add('description')
+            ->add('code')
+            ->getForm();
+
+        //Traitement du formulaire
+        $form->handleRequest($request);
+
+        //Si le form a des information
+        if($form->isSubmitted() && $form->isValid()){
+            $musique->setCreated(new \DateTime());
+
+            $manager->persist($musique);     // Persister les donnés
+            $manager->flush();              //Envoie des donnée
+
+            //Redirection vaire la musique Ajouter
+            return $this->redirectToRoute('musique_detail', ['id' => $musique->getId()]);
+        }
+
+        return $this->render("musique/create.html.twig",[
+            'current_menu' => 'musique_new',
+            'formMusique' => $form->createView()
+        ]);
+    }
+
     /**
      * @Route("/musique/{id}", name="musique_detail")
      */
-    public function details(MusiqueRepository $repo,$id)
-    {
-        $musiques = $repo->find($id);
-
+    public function details(Musique $musiques){
         return $this->render("musique/detail.html.twig",[
             "musique" => $musiques
         ]);
