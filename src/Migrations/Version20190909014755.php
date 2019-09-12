1eb19f584337b483cc6aa1fa4fd8797a7139826e
jahleel <vabyz971@gmail.com>
2019-09-12 11:11:14 -0400

    correction de la migration
    id_musique_id => id_user_id

--- src/Migrations/Version20190909014755.php
+++ src/Migrations/Version20190909014755.php
@@ -12,28 +12,28 @@ use Doctrine\Migrations\AbstractMigration;
  */
 final class Version20190909014755 extends AbstractMigration
 {
-    public function getDescription() : string
+    public function getDescription(): string
     {
         return '';
     }
 
-    public function up(Schema $schema) : void
+    public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE musique ADD id_musique_id INT DEFAULT NULL');
-        $this->addSql('ALTER TABLE musique ADD CONSTRAINT FK_EE1D56BC8068D476 FOREIGN KEY (id_musique_id) REFERENCES user (id)');
-        $this->addSql('CREATE INDEX IDX_EE1D56BC8068D476 ON musique (id_musique_id)');
+        $this->addSql('ALTER TABLE musique ADD id_user_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE musique ADD CONSTRAINT FK_EE1D56BC8068D476 FOREIGN KEY (id_user_id) REFERENCES user (id)');
+        $this->addSql('CREATE INDEX IDX_EE1D56BC8068D476 ON musique (id_user_id)');
     }
 
-    public function down(Schema $schema) : void
+    public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE musique DROP FOREIGN KEY FK_EE1D56BC8068D476');
         $this->addSql('DROP INDEX IDX_EE1D56BC8068D476 ON musique');
-        $this->addSql('ALTER TABLE musique DROP id_musique_id');
+        $this->addSql('ALTER TABLE musique DROP id_user_id');
     }
 }
