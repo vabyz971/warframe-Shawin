d71e9aac933272a69b08c6b597077ee26457f9b2
jahleel <vabyz971@gmail.com>
2019-10-22 20:56:07 -0400

    Affiche les user sur la page Home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\MusiqueRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,20 +16,22 @@ class HomeController extends AbstractController
      * @Route("/", name="home")
      * @param MusiqueRepository $repository
      */
-    public function index(MusiqueRepository $repo)
+    public function index(MusiqueRepository $repoMusique, UserRepository $repoUser)
     {
         //Affiche les musique les plus récents
-        $musiques_recent = $repo->findBy([], ['created' => 'DESC']);
+        $musiques_recent = $repoMusique->findBy([], ['created' => 'DESC']);
 
         //Affiche les musique les plus aimer
 
         //Afficher les utilisateurs ou leur musique sont les plus comsulter
+        $users_best = $repoUser->findAll(); // Affiche tout les utilisateurs
 
 
 
         return $this->render('home/index.html.twig', [
             'current_menu' => 'home',
             'musiques' => $musiques_recent,
+            'users' => $users_best
 
         ]);
     }
